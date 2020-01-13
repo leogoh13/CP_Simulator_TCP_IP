4b0c4aefda7994a8ca432fc4b7fb6d5fb71b027a
leogoh13 <leogoh95@gmail.com>
2020-01-13 10:56:08 +0800

    Add project files.

---
+++ CP_Simulator_TCP_IP/Carpark.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+
+
+namespace CP_Simulator_TCP_IP
+{
+    partial class Program
+    {
+        class Carpark
+        {
+            public enum TerminalTypes
+            {
+                SupportTerminal,
+                ComplementaryTerminal
+            }
+
+            public Carpark() // Terminaltypes
+            {
+
+            }
+
+            public byte[] GetNormalFare() // Combine with complementary or separate?
+            {
+                // For now the reply for both support terminal and complementary terminal
+                // is the same.
+                int[] randAmountsInCents = new int[]
+                {1000, 150, 250, 500, 10000, 15000, 5000, 1500, 300, 450,
+                    1250, 350, 900, 600, 1300, 2500, 7000, 50000, 4500, 3000};
+                Random rand = new Random();
+                int min = 0;
+                int max = 20;
+                int randAmountRange = rand.Next(min, max - 1);
+                int randAmountSelected = randAmountsInCents[randAmountRange];
+                string status = string.Empty;
+                string sysTime = Utils.GetSystemTime();
+
+                status += Commands.successStatus;
+                status += Encoding.ASCII.GetString(Utils.Int2BCD(randAmountSelected, 4));
+                status += Encoding.ASCII.GetString(Utils.Str2BCD(sysTime));
+                status += Commands.carriageReturn;
+
+                Console.WriteLine("\n\nGet Fare >>");
+                return Utils.Str2Byte(status);
+            }
+            public byte[] GetComplementaryFare() // Combine with complementary or separate?
+            {
+                // For now the reply for both support terminal and complementary terminal
+                // is the same.
+                int[] randAmounts = new int[]
+                {5000, 10000};
+                Console.WriteLine("\n\n<< Get Fare >>");
+                Random rand = new Random();
+                int min = 0;
+                int max = 2;
+                int randAmount = rand.Next(min, max - 1);
+                string status = null;
+
+                status += Commands.successStatus;
+                status += Encoding.ASCII.GetString(Utils.Int2BCD(randAmounts[randAmount], 4));
+                status += Commands.carriageReturn;
+
+                return Encoding.ASCII.GetBytes(status);
+            }
+            public byte[] OpenGate2_0(string data)
+            {
+                Console.WriteLine("\n\nOpen Gate >>");
+                // For now, just return success
+                return Encoding.ASCII.GetBytes(Commands.successStatusFull);
+            }
+            public void ShowData(byte[] value)
+            {
+                string temp = BitConverter.ToString(value);
+                temp = temp.Replace("-", string.Empty);
+                Console.WriteLine("{0}", temp);
+            }
+        }
+    }
+}
---
+++ CP_Simulator_TCP_IP/Utils.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+
+namespace CP_Simulator_TCP_IP
+{
+    static class Utils
+    {
+        public static byte[] Int2BCD(int number, int bcdByteLen)
+        {
+            string numberString = number.ToString().PadLeft(bcdByteLen * 2, '0');
+            byte[] numberBCD = new byte[bcdByteLen];
+            byte[] temp = null;
+
+            for (int i = 0; i < bcdByteLen; i++)
+            {
+                string oneByte = numberString.Substring(i * 2, 2);
+                int high = (int.Parse(oneByte[0].ToString()) << 4);
+                high = high + 15;
+                int low = int.Parse(oneByte[1].ToString());
+                low = low + 240;
+
+                int BCDformat = high & low;
+                temp = BitConverter.GetBytes(BCDformat);
+                numberBCD[i] = temp[0];
+            }
+            return numberBCD;
+        }
+        public static string Hex2Ascii(string hexStr)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexStr.Length; i += 2)
+                {
+                    string hs = string.Empty;
+
+                    hs = hexStr.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+        public static string Ascii2Hex(byte[] asciiStr)
+        {
+            try
+            {
+                string temp = BitConverter.ToString(asciiStr);
+                temp.Replace('-', '\x00');
+                return BitConverter.ToString(asciiStr);
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+
+            return string.Empty;
+        }
+        public static byte[] Str2Byte(string value)
+        {
+            return Encoding.ASCII.GetBytes(value);
+        }
+        public static string Byte2StrAscii(byte[] value)
+        {
+            return Encoding.ASCII.GetString(value);
+        }
+        public static string GetSystemTime()
+        {
+            string systime = DateTime.Now.ToString("yyMMddhhmmss");
+            return systime;
+        }
+        public static byte[] Str2BCD(string value)
+        {
+            var bcd = new byte[value.Length / 2];
+            for (var i = 0; i < bcd.Length; i++)
+            {
+                bcd[i] = Convert.ToByte(value.Substring(i * 2, 2), 16);
+            }
+            return bcd;
+        }
+        public static string BCD2Str(byte[] bcd)
+        {
+            return Encoding.ASCII.GetString(bcd);
+        }
+        public static string Byte2StrHex(byte[] value)
+        {
+            string temp = BitConverter.ToString(value);
+            temp = temp.Replace("-", string.Empty);
+            int lenOfEOF = temp.LastIndexOf("0D") + 2;
+            return temp.Substring(0, lenOfEOF);
+        }
+    }
+}
